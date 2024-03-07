2ee95001bffb76b4ce914ef8c230982774b09cb7
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-03-07 11:39:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,29 @@ struct vector2D{
     double x=0;
     double y=0;
 
+    void print();
     void scale(double val);
+    void rotate(double angle);
+    void normalize();
     void add(vector2D v);
     void subtract(vector2D v);
     double dot(vector2D v);
-    void normalize();
 };
 
-//Member functions
+//2D member functions
+void vector2D::print(){
+    printf("[%lf, %lf]\n",x,y);
+}
 void vector2D::scale(double val){
     x*=val;
     y*=val;
+}
+void vector2D::rotate(double angle){
+    angle = angle * M_PI / 180; // convert to degrees
+    double cx = x;
+    double cy = y;
+    x=(cx*cos(angle)) - (cy*sin(angle));
+    y=(cx*sin(angle)) + (cy*cos(angle));
 }
 void vector2D::normalize(){
     double m = sqrt(x*x + y*y);
@@ -34,5 +46,12 @@ void vector2D::subtract(vector2D v){
 double vector2D::dot(vector2D v){
     return (x*v.x) + (y*v.y);
 }
+
 int main(){
+    vector2D test;
+    test.x=4;
+    test.y=-2;
+    test.print();
+    test.rotate(45);
+    test.print();
 }
