ebf9a2745be418334d35b8786c673a9cdec9c965
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-03-07 10:52:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,19 @@ struct vector2D{
     void scale(double val);
     void add(vector2D v);
     void subtract(vector2D v);
-    int dot(vector2D v);
+    double dot(vector2D v);
+    void normalize();
 };
-//member fucntions
+
+//Member functions
 void vector2D::scale(double val){
     x*=val;
     y*=val;
+}
+void vector2D::normalize(){
+    double m = sqrt(x*x + y*y);
+    x/=m;
+    y/=m;
 }
 void vector2D::add(vector2D v){
     x+=v.x;
@@ -24,7 +31,7 @@ void vector2D::subtract(vector2D v){
     x+=v.x*(-1);
     y+=v.y*(-1);
 }
-int vector2D::dot(vector2D v){
+double vector2D::dot(vector2D v){
     return (x*v.x) + (y*v.y);
 }
 int main(){
