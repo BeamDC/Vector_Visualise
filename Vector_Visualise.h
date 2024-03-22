f03c44b5bc8ff05b4bc6f969b81213e1bd5eeffa
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-03-22 09:56:23 -0400

    Update Vector_Visualise.h
    -Small optimization
    -added getEquation function for planes
    -added getIntercepts function for planes

--- Vector_Visualise.h
+++ Vector_Visualise.h
@@ -5,45 +5,31 @@
 
 //Types
 struct point3D{
-    double x;
-    double y;
-    double z;
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
 };
 struct vector3D{
-    double x;
-    double y;
-    double z;
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
 };
 struct plane{
-    //original points
     point3D pointA;
     point3D pointB;
     point3D pointC;
 
-    //intercepts 
-    point3D x_int{1,0,0};
-    point3D y_int{0,1,0};
-    point3D z_int{0,0,1};
-
-    //equation values
-    double a;
-    double b;
-    double c;
-    double d;
-
-    //normal vector
-    //(unit vector perpendicular to the plane)
     vector3D normal{};
 };
 
-/** 3D point functions **/
+/** point functions **/
 void print(point3D p){
-    printf("(%f, %f, %f)",p.x,p.y,p.z);
+    printf("(%f, %f, %f)\n",p.x,p.y,p.z);
 }
 point3D rotate(point3D p, double yaw, double pitch, double roll){
     point3D r{};
 
-    double DegToRad = M_PI / -180;
+    double DegToRad = M_PI / 180;
     yaw *= DegToRad;
     pitch *= DegToRad;
     roll *= DegToRad;
@@ -74,7 +60,7 @@ point3D rotate(point3D p, double yaw, double pitch, double roll){
     return r;
 }
 
-/** 3D vector functions **/
+/** vector functions **/
 void print(vector3D v){
     printf("[%f, %f, %f]\n",v.x,v.y,v.z);
 }
@@ -84,40 +70,6 @@ double magnitude(vector3D v){
 }
 double dot(vector3D v, vector3D w){
     return (v.x*w.x) + (v.y*w.y) + (v.z*w.z);
-}
-
-vector3D rotate(vector3D v, double yaw, double pitch, double roll){
-    vector3D r{};
-
-    double DegToRad = M_PI / -180;
-    yaw *= DegToRad;
-    pitch *= DegToRad;
-    roll *= DegToRad;
-
-    double yawCos = cos(yaw);
-    double pitchCos = cos(pitch);
-    double rollCos = cos(roll);
-    double yawSin = sin(yaw);
-    double pitchSin = sin(pitch);
-    double rollSin = sin(roll);
-
-    double xx = yawCos * pitchCos;
-    double xy = yawCos * pitchSin * rollSin - yawSin * rollCos;
-    double xz = yawCos * pitchSin * rollCos + yawSin * rollSin;
-
-    double yx = yawSin * pitchCos;
-    double yy = yawSin * pitchSin * rollSin + yawCos * rollCos;
-    double yz = yawSin * pitchSin * rollCos - yawCos * rollSin;
-
-    double zx = -pitchSin;
-    double zy = pitchCos * rollSin;
-    double zz = pitchCos * rollCos;
-
-    r.x = xx * v.x + xy * v.y + xz * v.z;
-    r.y = yx * v.x + yy * v.y + yz * v.z;
-    r.z = zx * v.x + zy * v.y + zz * v.z;
-
-    return r;
 }
 
 vector3D construct(point3D a, point3D b){
@@ -163,30 +115,61 @@ vector3D cross(vector3D v, vector3D w){
     r.z = (v.x * w.y) - (w.x * v.y);
     return r;
 }
+vector3D rotate(vector3D v, double yaw, double pitch, double roll){
+    vector3D r{};
+
+    double DegToRad = M_PI / 180;
+    yaw *= DegToRad;
+    pitch *= DegToRad;
+    roll *= DegToRad;
+
+    double yawCos = cos(yaw);
+    double pitchCos = cos(pitch);
+    double rollCos = cos(roll);
+    double yawSin = sin(yaw);
+    double pitchSin = sin(pitch);
+    double rollSin = sin(roll);
+
+    double xx = yawCos * pitchCos;
+    double xy = yawCos * pitchSin * rollSin - yawSin * rollCos;
+    double xz = yawCos * pitchSin * rollCos + yawSin * rollSin;
+
+    double yx = yawSin * pitchCos;
+    double yy = yawSin * pitchSin * rollSin + yawCos * rollCos;
+    double yz = yawSin * pitchSin * rollCos - yawCos * rollSin;
+
+    double zx = -pitchSin;
+    double zy = pitchCos * rollSin;
+    double zz = pitchCos * rollCos;
+
+    r.x = xx * v.x + xy * v.y + xz * v.z;
+    r.y = yx * v.x + yy * v.y + yz * v.z;
+    r.z = zx * v.x + zy * v.y + zz * v.z;
+
+    return r;
+}
 
 /** PLANES **/
 void print(plane P){
-    print(P.normal);printf("\n");
-    print(P.pointA);printf("\n");
-    print(P.pointB);printf("\n");
-    print(P.pointC);printf("\n");
+    printf("Normal:  (%f, %f, %f)\n\n",P.normal.x,P.normal.y,P.normal.z);
+    printf("Point A: (%f, %f, %f)\n",P.pointA.x,P.pointA.y,P.pointA.z);
+    printf("Point B: (%f, %f, %f)\n",P.pointB.x,P.pointB.y,P.pointB.z);
+    printf("Point C: (%f, %f, %f)\n",P.pointC.x,P.pointC.y,P.pointC.z);
 }
 
 plane construct(point3D A, point3D B, point3D C){
     plane r{};
-    vector3D AB = construct(A,B);
-    vector3D AC = construct(A,C);
 
     r.pointA = A;
     r.pointB = B;
     r.pointC = C;
     
-    //normal vector
+    vector3D AB = construct(A,B);
+    vector3D AC = construct(A,C);
     r.normal = normalize(cross(AB,AC));
     
     return r;
 }
-
 plane rotate(plane P, double yaw, double pitch, double roll){
     plane r{};
 
@@ -196,6 +179,26 @@ plane rotate(plane P, double yaw, double pitch, double roll){
     r.pointC = rotate(P.pointC,yaw,pitch,roll);
 
     return r;
+}
+double* getEquation(plane P){
+    static double eqn[4];
+    
+    eqn[0] = P.normal.x;
+    eqn[1] = P.normal.y;
+    eqn[2] = P.normal.z;
+    eqn[3] = -P.normal.x * P.pointA.x + P.normal.y * P.pointA.y + P.normal.z * P.pointA.z;
+
+    return eqn;
+}
+point3D* getIntercepts(plane P){
+    static point3D xyz_ints[3];
+    double* e = getEquation(P);
+
+    xyz_ints[0].x = -(e[3]/e[0]);
+    xyz_ints[1].y = -(e[3]/e[1]);
+    xyz_ints[2].z = -(e[3]/e[2]);
+    
+    return xyz_ints;
 }
 
 #endif //VECTOR_VISUALISE_H
