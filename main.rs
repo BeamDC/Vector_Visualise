1633efd5e5b631eb47165a70ced0f0c132bdb6a5
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-06-05 20:17:37 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,176 +1,138 @@
-use std::default::Default;
+mod math;
 
-#[derive(Debug, Clone, Copy)]
-struct Point3D{
-    x: f64,
-    y: f64,
-    z: f64,
+use std::fmt;
+use math::*;
+use egui::{ComboBox, DragValue};
+
+#[derive(PartialEq)]
+enum Types {
+    Point,
+    Vector,
+    Plane,
 }
 
-#[derive(Debug, Clone)]
-struct Vec3D{
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-#[derive(Debug, Clone)]
-struct Plane3D{
-    a: Point3D,
-    b: Point3D,
-    c: Point3D,
-
-    normal: Vec3D
-}
-
-/* Defaults */
-impl Default for Point3D {
-    fn default() -> Self {
-        Self { x: 0.0, y: 0.0, z: 0.0 }
-    }
-}
-
-impl Default for Vec3D {
-    fn default() -> Self {
-        Self { x: 0.0, y: 0.0, z: 0.0 }
-    }
-}
-
-impl Default for Plane3D {
-    fn default() -> Self {
-        Self {
-            a: Point3D{..Default::default()},
-            b: Point3D{..Default::default()},
-            c: Point3D{..Default::default()},
-            normal: Vec3D{..Default::default()}
+impl fmt::Display for Types {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Types::Point => write!(f, "Point"),
+            Types::Vector => write!(f, "Vector"),
+            Types::Plane => write!(f, "Plane"),
         }
     }
 }
 
-/* Vector Functions */
-fn magnitude(v: Vec3D) -> f64{
-    (v.x * v.x + v.y * v.y + v.z * v.z).sqrt()
+struct MainWindow {
+    add_type: Types,
+    standard_point: Point3D,
+    standard_vector: Vec3D,
+    standard_plane: Plane3D,
+    points: Vec<Point3D>,
+    vectors: Vec<Vec3D>,
+    planes: Vec<Plane3D>,
 }
 
-fn dot(v: Vec3D, w: Vec3D) -> f64{
-    v.x * w.x + v.y * w.y + v.z * w.z
+impl Default for MainWindow {
+    fn default() -> Self {
+        Self {
+            add_type: Types::Point,
+            standard_point: Point3D { ..Default::default() },
+            standard_vector: Vec3D { ..Default::default() },
+            standard_plane: Plane3D { ..Default::default() },
+            points: Vec::new(),
+            vectors: Vec::new(),
+            planes: Vec::new(),
+        }
+    }
 }
 
-fn construct_vec(a: Point3D, b: Point3D) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    r.x = b.x - a.x;
-    r.y = b.y - a.y;
-    r.z = b.z - a.z;
-    r
+impl eframe::App for MainWindow {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                //dropdown menu for types
+                ui.label("Add New Item: ");
+                ComboBox::from_label("")
+                    .selected_text(format!("{}", self.add_type))
+                    .show_ui(ui, |ui| {
+                        ui.style_mut().wrap = Some(false);
+                        ui.set_min_width(45.0);
+                        ui.selectable_value(&mut self.add_type, Types::Point, Types::Point.to_string());
+                        ui.selectable_value(&mut self.add_type, Types::Vector, Types::Vector.to_string());
+                        ui.selectable_value(&mut self.add_type, Types::Plane, Types::Plane.to_string());
+                    });
+            });
+
+            // inputs for each type
+            match self.add_type {
+                Types::Point => ui.horizontal(|ui| {
+                    ui.label("X: ");
+                    ui.add(DragValue::new(&mut self.standard_point.x).speed(0.5));
+                    ui.label("Y: ");
+                    ui.add(DragValue::new(&mut self.standard_point.y).speed(0.5));
+                    ui.label("Z: ");
+                    ui.add(DragValue::new(&mut self.standard_point.z).speed(0.5));
+                }),
+                Types::Vector => ui.horizontal(|ui| {
+                    ui.label("X: ");
+                    ui.add(DragValue::new(&mut self.standard_vector.x).speed(0.5));
+                    ui.label("Y: ");
+                    ui.add(DragValue::new(&mut self.standard_vector.y).speed(0.5));
+                    ui.label("Z: ");
+                    ui.add(DragValue::new(&mut self.standard_vector.z).speed(0.5));
+                }),
+                Types::Plane => ui.vertical(|ui| {
+                    ui.horizontal(|ui|{
+                        ui.label("X1: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.a.x).speed(0.5));
+                        ui.label("Y1: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.a.y).speed(0.5));
+                        ui.label("Z1: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.a.z).speed(0.5));
+                    });
+                    ui.horizontal(|ui|{
+                        ui.label("X2: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.b.x).speed(0.5));
+                        ui.label("Y2: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.b.y).speed(0.5));
+                        ui.label("Z2: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.b.z).speed(0.5));
+                    });
+                    ui.horizontal(|ui|{
+                        ui.label("X3: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.c.x).speed(0.5));
+                        ui.label("Y3: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.c.y).speed(0.5));
+                        ui.label("Z3: ");
+                        ui.add(DragValue::new(&mut self.standard_plane.c.z).speed(0.5));
+                    });
+                }),
+            };
+
+            //button to construct the type
+            if ui.button("CONSTRUCT").clicked(){
+                match self.add_type {
+                    Types::Point => self.points.push(self.standard_point),
+                    Types::Vector => self.vectors.push(self.standard_vector),
+                    Types::Plane => self.planes.push(self.standard_plane),
+                }
+                println!("Points: {:#?}",self.points);
+                println!("Vectors: {:#?}",self.vectors);
+                println!("Planes: {:#?}",self.planes);
+            }
+        });
+    }
 }
 
-fn add(v: Vec3D, w: Vec3D) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    r.x = v.x + w.x;
-    r.y = v.y + w.y;
-    r.z = v.z + w.z;
-    r
-}
 
-fn opposite(v: Vec3D) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    r.x = -v.x;
-    r.y = -v.y;
-    r.z = -v.z;
-    r
-}
-
-fn normalize(v: Vec3D) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    let m:f64 = magnitude(v.clone());
-    r.x = v.x / m;
-    r.y = v.y / m;
-    r.z = v.z / m;
-    r
-}
-
-fn scale(v: Vec3D, scalar: f64) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    r.x = v.x * scalar;
-    r.y = v.y * scalar;
-    r.z = v.z * scalar;
-    r
-}
-
-fn cross(v: Vec3D, w: Vec3D) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    r.x = (v.y * w.z) - (v.z * w.y);
-    r.y = (v.z * w.x) - (v.x * w.z);
-    r.z = (v.x * w.y) - (v.y * w.x);
-    r
-}
-
-fn rotate(v: Vec3D, yaw: f64, pitch: f64, roll: f64) -> Vec3D{
-    let mut r = Vec3D{..Default::default()};
-    let yaw_cos: f64 = yaw.cos();
-    let yaw_sin: f64 = yaw.sin();
-    let pitch_cos: f64 = pitch.cos();
-    let pitch_sin: f64 = pitch.sin();
-    let roll_cos: f64 = roll.cos();
-    let roll_sin: f64 = roll.sin();
-
-    let xx = yaw_cos * pitch_cos;
-    let xy = yaw_cos * pitch_sin * roll_sin - yaw_sin * roll_cos;
-    let xz = yaw_cos * pitch_sin * roll_cos + yaw_sin * roll_sin;
-
-    let yx = yaw_sin * pitch_cos;
-    let yy = yaw_sin * pitch_sin * roll_sin + yaw_cos * roll_cos;
-    let yz = yaw_sin * pitch_sin * roll_cos - yaw_cos * roll_sin;
-
-    let zx = -pitch_sin;
-    let zy = pitch_cos * roll_sin;
-    let zz = pitch_cos * roll_cos;
-
-    r.x = xx * v.x + xy * v.y + xz * v.z;
-    r.y = yx * v.x + yy * v.y + yz * v.z;
-    r.z = zx * v.x + zy * v.y + zz * v.z;
-
-    r
-}
-
-/* Plane Functions */
-fn construct_plane(a: Point3D, b: Point3D, c: Point3D) -> Plane3D{
-    let mut r = Plane3D{..Default::default()};
-    r.a = a.clone();
-    r.b = b.clone();
-    r.c = c.clone();
-    let ab:Vec3D = construct_vec(a.clone(), b);
-    let ac:Vec3D = construct_vec(a, c);
-    r.normal = normalize(cross(ab, ac));
-    r
-}
-
-fn get_equation(p: Plane3D) -> [f64; 4]{
-    let mut eqn: [f64; 4] = [0.0; 4];
-
-    eqn[0] = p.normal.x;
-    eqn[1] = p.normal.y;
-    eqn[2] = p.normal.z;
-    eqn[3] = -p.normal.x * p.a.x + p.normal.y * p.a.y + p.normal.z * p.a.z;
-
-    eqn
-}
-
-fn get_intercepts(p: Plane3D) -> [Point3D; 3]{
-    let mut xyz_ints: [Point3D; 3] = [Point3D{..Default::default()}; 3];
-
-    let e = get_equation(p);
-    xyz_ints[0].x = -(e[3]/e[0]);
-    xyz_ints[1].x = -(e[3]/e[1]);
-    xyz_ints[2].x = -(e[3]/e[2]);
-
-    xyz_ints
-}
-
-fn main(){
-    let a = Point3D{x: 1.0,y: 1.0,z: 1.0};
-    let b = Point3D{x: 2.0,y: 7.0,z: 2.0};
-    let c = Point3D{x: 2.0,y: 8.0,z: 7.0};
-    let p: Plane3D = construct_plane(a,b,c);
-    println!("{:#?}",p);
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
+        ..Default::default()
+    };
+    eframe::run_native("Vector Visualiser", options,
+                       Box::new(|cc| {
+                           Box::<MainWindow>::default()
+                       }),
+    )
 }
